557835dbf0170db1225b1bc0db0647bf96cc1e96
Max Kellermann <max.kellermann@gmail.com>
2019-11-19 12:53:55 +0100

    web/map.js: highlight the current location

--- web/map.js
+++ web/map.js
@@ -5,6 +5,17 @@ var gpx_style = new ol.style.Style({
   })
 });
 
+var locationStyle = new ol.style.Style({
+  image: new ol.style.Circle({
+    radius: 5,
+    fill: null,
+    stroke: new ol.style.Stroke({
+      color: 'rgba(255,50,50,0.8)',
+      width: 2
+    })
+  })
+});
+
 var vectorLayer = new ol.layer.Vector({
   source: new ol.source.Vector({
     url: '/gpx/42.gpx',
@@ -13,6 +24,14 @@ var vectorLayer = new ol.layer.Vector({
 
   style: gpx_style
 })
+
+vectorLayer.on('postrender', function(evt) {
+  var currentCoordinate = vectorLayer.getSource().getFeatures()[0].getGeometry().getLastCoordinate();
+  var point = new ol.geom.Point(currentCoordinate);
+  var vectorContext = ol.render.getVectorContext(evt);
+  vectorContext.setStyle(locationStyle);
+  vectorContext.drawGeometry(point);
+});
 
 var map = new ol.Map({
   target: 'map',
