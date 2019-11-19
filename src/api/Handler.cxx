f53a21788d714f03ff9c0de9527c3ccfaa86e779
Max Kellermann <max.kellermann@gmail.com>
2019-11-19 20:29:30 +0100

    api/Main: move code to Handler.cxx

--- src/api/Handler.cxx
+++ src/api/Handler.cxx
@@ -29,56 +29,27 @@
  */
 
 #include "Handler.hxx"
+#include "Response.hxx"
+#include "GetGPX.hxx"
 #include "pg/Connection.hxx"
-#include "util/PrintException.hxx"
-#include "config.h"
+#include "util/StringCompare.hxx"
 
-#ifdef HAVE_LIBSYSTEMD
-#include <systemd/sd-daemon.h>
-#endif
+void
+HandleRequest(Pg::Connection &db,
+	      FCGX_Stream *in, FCGX_Stream *out, FCGX_Stream *err,
+	      FCGX_ParamArray envp) noexcept
+{
+	(void)in;
+	(void)err;
 
-#include <fastcgi.h>
-#include <fcgiapp.h>
-
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int
-main(int, const char *const*) noexcept
-try {
-#ifdef HAVE_LIBSYSTEMD
-	/* support systemd socket activation by copying systemd's fd
-	   to stdin */
-	int n = sd_listen_fds(true);
-	if (n > 0) {
-		if (n != 1)
-			throw "Too many systemd fds";
-
-		if (dup3(SD_LISTEN_FDS_START, FCGI_LISTENSOCK_FILENO, O_CLOEXEC) < 0)
-			throw "dup2() failed";
-		close(SD_LISTEN_FDS_START);
+	const char *const path_info = FCGX_GetParam("PATH_INFO", envp);
+	if (path_info == nullptr) {
+		NotFound(out);
+		return;
 	}
-#endif
 
-	const char *_db = "dbname=beacon"; // TODO make configurable
-	Pg::Connection db(_db);
-
-	FCGX_Init();
-	FCGX_Request request;
-	FCGX_InitRequest(&request, 0, 0);
-
-#ifdef HAVE_LIBSYSTEMD
-	/* tell systemd we're ready */
-	sd_notify(0, "READY=1");
-#endif
-
-	while (FCGX_Accept_r(&request) == 0)
-		HandleRequest(db, request.in, request.out, request.err,
-			      request.envp);
-
-	return EXIT_SUCCESS;
-} catch (...) {
-	PrintException(std::current_exception());
-	return EXIT_FAILURE;
+	if (auto gpx = StringAfterPrefix(path_info, "gpx/"))
+		HandleGPX(db, gpx, out, envp);
+	else
+		NotFound(out);
 }
