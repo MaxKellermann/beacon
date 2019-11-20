dc7dea018df72a3908ddf42865599a3e240e0c8e
Max Kellermann <max.kellermann@gmail.com>
2019-11-20 15:40:29 +0100

    web/index.html: show track list in combo box

---
+++ web/menu.js
@@ -0,0 +1,26 @@
+function switchTrack(control) {
+  let id = control.options[control.selectedIndex].value;
+  map.removeLayer(vectorLayer);
+  vectorLayer = loadTrackLayer(id);
+  map.addLayer(vectorLayer);
+}
+
+function updateTrackList(control) {
+  get('/api/list', function(data) {
+    list = JSON.parse(data)
+
+    control.innerHTML = '<option value="">none</option>'
+    for (let i = 0; i < list.length; ++i) {
+      let e = document.createElement("option");
+      e.setAttribute("value", list[i]['id'])
+      e.innerText = list[i]['id'];
+      control.appendChild(e);
+    }
+  })
+}
+
+function reloadTrackList() {
+  updateTrackList(document.getElementById('track'));
+}
+
+window.onload = reloadTrackList;
