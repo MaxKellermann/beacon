918393e460241c551d15dcc642d39c7d92b8fff8
Max Kellermann <max.kellermann@ionos.com>
2026-01-17 13:45:41 +0100

    event/PipeEvent: add more SocketEvent method wrappers

--- src/event/PipeEvent.hxx
+++ src/event/PipeEvent.hxx
@@ -84,4 +84,16 @@ public:
 	void ScheduleImplicit() noexcept {
 		event.ScheduleImplicit();
 	}
+
+	void ScheduleAnyHangup() noexcept {
+		event.ScheduleAnyHangup();
+	}
+
+	bool IsReadPending() const noexcept {
+		return event.IsReadPending();
+	}
+
+	bool IsWritePending() const noexcept {
+		return event.IsWritePending();
+	}
 };
